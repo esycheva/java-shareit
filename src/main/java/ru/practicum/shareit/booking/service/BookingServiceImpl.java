fd1d67223d4a246db3fa602725dc55f936ae4532
Истомина Елена <69istominaea@region.cbr.ru>
2026-06-23 20:43:10 +0300

    fix for checks

--- src/main/java/ru/practicum/shareit/booking/service/BookingServiceImpl.java
+++ src/main/java/ru/practicum/shareit/booking/service/BookingServiceImpl.java
@@ -1,5 +1,5 @@
 package ru.practicum.shareit.booking.service;
-;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
