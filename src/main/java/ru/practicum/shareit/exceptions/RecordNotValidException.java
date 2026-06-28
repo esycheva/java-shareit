1b3096b308f344c6a7e67c3ce13673b869fa6f5a
Истомина Елена <69istominaea@region.cbr.ru>
2026-06-28 09:06:30 +0300

    разделение приложения на два

--- src/main/java/ru/practicum/shareit/exceptions/RecordNotValidException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.exceptions;
-
-public class RecordNotValidException extends RuntimeException {
-    public RecordNotValidException(String message) {
-        super(message);
-    }
-}
