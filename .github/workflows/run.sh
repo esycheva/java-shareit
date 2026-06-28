a648b226f1a1d34cda8302fe56b6a03febc9cd3e
Истомина Елена <69istominaea@region.cbr.ru>
2026-06-29 01:56:29 +0300

    fix docker-compose.yml

--- .github/workflows/run.sh
+++ .github/workflows/run.sh
@@ -1,10 +1,28 @@
+#!/bin/bash
+
 chmod a+x ./tests/.github/workflows/wait-for-it.sh
-docker compose -f docker-compose.yml up --detach  &&
-echo "Docker is up" &&
-./tests/.github/workflows/wait-for-it.sh -t 120 server:9090 &&
-echo "Server is up" &&
-./tests/.github/workflows/wait-for-it.sh -t 120 gateway:8080 &&
-echo "Gateway is up"
-result=$?
+
+docker compose -f docker-compose.yml up -d
+echo "Docker is up"
+
+./tests/.github/workflows/wait-for-it.sh -t 120 localhost:9090
+server_result=$?
+
+if [ $server_result -ne 0 ]; then
+  docker compose -f docker-compose.yml logs
+  exit $server_result
+fi
+
+echo "Server is up"
+
+./tests/.github/workflows/wait-for-it.sh -t 120 localhost:8080
+gateway_result=$?
+
 docker compose -f docker-compose.yml logs
-exit $result
+
+if [ $gateway_result -ne 0 ]; then
+  exit $gateway_result
+fi
+
+echo "Gateway is up"
+exit 0
