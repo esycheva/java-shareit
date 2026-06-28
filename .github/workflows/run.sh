d57e4d435333c9b75ae800d890076ff15a2f0eda
Истомина Елена <69istominaea@region.cbr.ru>
2026-06-29 01:03:59 +0300

    поправлен файл run.sh

--- .github/workflows/run.sh
+++ .github/workflows/run.sh
@@ -1,9 +1,9 @@
 chmod a+x ./tests/.github/workflows/wait-for-it.sh
 docker compose -f docker-compose.yml up --detach  &&
 echo "Docker is up" &&
-./tests/.github/workflows/wait-for-it.sh -t 60 localhost:9090 &&
+./tests/.github/workflows/wait-for-it.sh -t 60 server:9090 &&
 echo "Server is up" &&
-./tests/.github/workflows/wait-for-it.sh -t 60 localhost:8080 &&
+./tests/.github/workflows/wait-for-it.sh -t 60 gateway:8080 &&
 echo "Gateway is up"
 result=$?
 docker compose -f docker-compose.yml logs
