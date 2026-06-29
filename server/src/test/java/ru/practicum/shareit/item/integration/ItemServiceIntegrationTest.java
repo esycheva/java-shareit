ea04f4807d4f200ed2cadc171a833f8df1ff2806
Истомина Елена <69istominaea@region.cbr.ru>
2026-06-29 04:55:27 +0300

    fix for checks

--- server/src/test/java/ru/practicum/shareit/item/integration/ItemServiceIntegrationTest.java
+++ server/src/test/java/ru/practicum/shareit/item/integration/ItemServiceIntegrationTest.java
@@ -11,7 +11,6 @@ import ru.practicum.shareit.user.dto.UserDto;
 import ru.practicum.shareit.user.service.UserService;
 
 import java.util.Collection;
-import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
